ec647af6c79d1e583c4919b9dcd28b989762f70f
Andriy Izbash <andreyizbash228@gmail.com>
2023-09-17 14:39:40 +0300

    add test 2 lab

--- LabWork/mobileApp/firstLab/firstLab/View/TextView.swift
+++ LabWork/mobileApp/firstLab/firstLab/View/TextView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UserNotificationsUI
 
 struct TextView: View
 {
---
+++ LabWork/mobileApp/secondLab/secondLab/secondLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondLabApp.swift
+//  secondLab
+//
+//  Created by Андрiй on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct secondLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
