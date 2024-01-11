406e63665583f54aff53294faf120ede7247c836
Andrii Izbash <104149691+AndriiBash@users.noreply.github.com>
2024-01-11 17:03:25 +0200

    add 1 lab oop

--- LabWork/ComputerGraphic/OpenGLC++ with textured/OpenGLC++/main.cpp
+++ LabWork/ComputerGraphic/OpenGLC++ with textured/OpenGLC++/main.cpp
@@ -2,239 +2,86 @@
 #include <math.h>
 #include <GLUT/glut.h>
 
-GLuint texture[2];
-float teapotRotation = 0.0;  // Кут обертання
+float angle = 0.0;
 
-struct Image {
-    unsigned long sizeX;
-    unsigned long sizeY;
-    char *data;
-};
-
-typedef struct Image Image;
-
-#define checkImageWidth 64
-#define checkImageHeight 64
-
-GLubyte checkImage[checkImageWidth][checkImageHeight][3];
-void makeCheckImage(void){
-    int i, j, c;
-    for (i = 0; i < checkImageWidth; i++) {
-        for (j = 0; j < checkImageHeight; j++) {
-            c = ((((i&0x8)==0)^((j&0x8)==0)))*255;
-            checkImage[i][j][0] = (GLubyte) c;
-            checkImage[i][j][1] = (GLubyte) c;
-            checkImage[i][j][2] = (GLubyte) c;
-        }
-    }
-}
-
-int ImageLoad(char *filename, Image *image) {
-    FILE *file;
-    unsigned long size; // size of the image in bytes.
-    unsigned long i; // standard counter.
-    unsigned short int planes; // number of planes in image (must be 1)
-    unsigned short int bpp; // number of bits per pixel (must be 24)
-    char temp; // temporary color storage for bgr-rgb conversion.
-    // make sure the file is there.
-    if ((file = fopen(filename, "rb"))==NULL){
-        printf("File Not Found : %s\n",filename);
-        return 0;
-    }
-    // seek through the bmp header, up to the width/height:
-    fseek(file, 18, SEEK_CUR);
-    // read the width
-    if ((i = fread(&image->sizeX, 4, 1, file)) != 1) {
-        printf("Error reading width from %s.\n", filename);
-        return 0;
-    }
-    //printf("Width of %s: %lu\n", filename, image->sizeX);
-    // read the height
-    if ((i = fread(&image->sizeY, 4, 1, file)) != 1) {
-        printf("Error reading height from %s.\n", filename);
-        return 0;
-    }
-    //printf("Height of %s: %lu\n", filename, image->sizeY);
-    // calculate the size (assuming 24 bits or 3 bytes per pixel).
-    size = image->sizeX * image->sizeY * 3;
-    // read the planes
-    if ((fread(&planes, 2, 1, file)) != 1) {
-        printf("Error reading planes from %s.\n", filename);
-        return 0;
-    }
-    if (planes != 1) {
-        printf("Planes from %s is not 1: %u\n", filename, planes);
-        return 0;
-    }
-    // read the bitsperpixel
-    if ((i = fread(&bpp, 2, 1, file)) != 1) {
-        printf("Error reading bpp from %s.\n", filename);
-        return 0;
-    }
-    if (bpp != 24) {
-        printf("Bpp from %s is not 24: %u\n", filename, bpp);
-        return 0;
-    }
-    // seek past the rest of the bitmap header.
-    fseek(file, 24, SEEK_CUR);
-    // read the data.
-    image->data = (char *) malloc(size);
-    if (image->data == NULL) {
-        printf("Error allocating memory for color-corrected image data");
-        return 0;
-    }
-    if ((i = fread(image->data, size, 1, file)) != 1) {
-        printf("Error reading image data from %s.\n", filename);
-        return 0;
-    }
-    for (i=0;i<size;i+=3) { // reverse all of the colors. (bgr -> rgb)
-        temp = image->data[i];
-        image->data[i] = image->data[i+2];
-        image->data[i+2] = temp;
-    }
-    // we're done.
-    return 1;
-}
-
-Image * loadTexture(){
-    Image *image1;
-    // allocate space for texture
-    image1 = (Image *) malloc(sizeof(Image));
-    if (image1 == NULL) {
-        printf("Error allocating space for image");
-        exit(0);
-    }
-    if (!ImageLoad("texture.bmp", image1))
-    {
-        exit(1);
-    }
-    return image1;
-}
-
-
-void myinit(void)
+void renderSphere()
 {
-    glClearColor (0.5, 0.5, 0.5, 0.0);
+    // Встановлюємо положення джерела світла
+    GLfloat light_position[] = { 1.0, 1.0, 2.0, 0.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    // Увімкнення освітлення
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    GLfloat ambient[] = { 0.64, 0.16, 1.00, 1.0 };
+    GLfloat diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
+    GLfloat specular[] = { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat shininess = 0.0;
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+
+    //glColor3f(1.0, 0.0, 0.0);  // Червоний колір для кулі
+
+    glPushMatrix();  // Зберігаємо поточну матрицю
+    glTranslated(2.0, 2.0, 0.0);  // Переміщуємо кулю
+    glRotated(angle, 0.0, 1.0, 0.0);  // Обертаємо кулю навколо Y-осі
+    glutSolidSphere(0.5, 50, 50);  // Малюємо кулю
+    glPopMatrix();  // Відновлюємо матрицю
+
+    // Вимкнення освітлення
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-    Image *image1 = loadTexture();
-    if(image1 == NULL){
-        printf("Image was not returned from loadTexture\n");
-        exit(0);
-    }
-    makeCheckImage();
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    // Create Texture
-    glGenTextures(2, texture);
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); //scale linearly when image bigger than texture
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); //scale linearly when image smalled than texture
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, image1->sizeX, image1->sizeY, 0,
-    GL_RGB, GL_UNSIGNED_BYTE, image1->data);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-    glBindTexture(GL_TEXTURE_2D, texture[1]);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, checkImageWidth,
-    checkImageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE,&checkImage[0][0][0]);
-    glEnable(GL_TEXTURE_2D);
-    glShadeModel(GL_FLAT);
 }
 
-void display(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void renderScene()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_DEPTH_BUFFER_BIT);
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // Огляд з камери
-
-    glPushMatrix();
-    glRotatef(teapotRotation, 0.0, 1.0, 0.0);  // Вращення торуса
-
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
-
-    const int numc = 50; // Кількість часткових коліць (кільць по горизонталі)
-    const int numt = 50; // Кількість часткових коліць (кільць по вертикалі)
-    const float R = 1.0; // Великий радіус торуса
-    const float r = 0.5; // Малий радіус торуса
-
-    for (int i = 0; i < numc; i++) {
-        glBegin(GL_QUAD_STRIP);
-        for (int j = 0; j <= numt; j++) {
-            for (int k = 0; k < 2; k++) {
-                float s = (i + k) % numc + 0.5;
-                float t = j % numt;
-
-                float x = (R + r * cos(s * 2.0 * M_PI / numc)) * cos(t * 2.0 * M_PI / numt);
-                float y = (R + r * cos(s * 2.0 * M_PI / numc)) * sin(t * 2.0 * M_PI / numt);
-                float z = r * sin(s * 2.0 * M_PI / numc);
-
-                float u = (i + k) / (float)numc;
-                float v = t / (float)numt;
-
-                glTexCoord2f(u, v);
-                glNormal3f(x, y, z);
-                glVertex3f(x, y, z);
-            }
-        }
-        glEnd();
-    }
-
-    glDisable(GL_TEXTURE_2D);
-
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
-
-
-void myReshape(int w, int h){
-    glViewport(0, 0, w, h);
+    // Встановлюємо тривимірну проєкцію
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60.0, 1.0*(GLfloat)w/(GLfloat)h, 1.0, 30.0);
+    gluPerspective(45.0, 1.0, 1.0, 100.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glTranslatef(0.0, 0.0, -3.6);
+
+    // Встановлення камери
+    gluLookAt(3.0, 3.0, 5.0, 2.0, 2.0, 0.0, 0.0, 1.0, 0.0);
+
+    renderSphere();
+
+    glFlush();
 }
 
-void keyboard (unsigned char key, int x, int y){
-    switch (key) {
-        case 27: // “esc” on keyboard
-        exit(0);
-        break;
-        default: // “a” on keyboard
-        break;
-    }
-}
-
-void update(int value){
-    teapotRotation += 1.0;  // Збільшуємо кут обертання чайника
-    if (teapotRotation >= 360.0) {
-        teapotRotation -= 360.0;  // Зациклюємо кут обертання
+void update(int value)
+{
+    angle += 1.0;  // Збільшуємо кут обертання
+    if (angle >= 360.0)
+    {
+        angle -= 360.0;  // Зациклюємо кут обертання
     }
 
-    glutPostRedisplay();  // Перемальовуємо сцену
-    glutTimerFunc(16, update, 0);  // Встановлюємо таймер для оновлення анімації
+    glutPostRedisplay();
+    glutTimerFunc(16, update, 0);
 }
 
-
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutCreateWindow("Смачний бублик");
-    myinit();
-    glutReshapeFunc (myReshape);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(keyboard);
-    
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(1800, 1600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Window name");
+
+    glEnable(GL_DEPTH_TEST);
+
+    glutDisplayFunc(renderScene);
     glutTimerFunc(0, update, 0);
     glutMainLoop();
     return 0;
 }
-
---
+++ LabWork/OOP/1/main.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  CommandLineToolC++
+//
+//  Created by Андрiй on 10.01.2024.
+//
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+template <typename type>
+type a(type parametr)
+{
+    return 3 * parametr - 2 * pow(cos(parametr), 2);
+}
+
+template <typename type>
+type b(type parametr)
+{
+    return log(parametr) + 2 * exp(parametr);
+}
+
+template <typename type>
+type c(type parametr)
+{
+    return pow(parametr, 1/3) + 4 * parametr - 1;
+}
+
+template <typename type>
+type h(type parametr)
+{
+    return 3 * a(parametr) + 4 * b(parametr) - 8;
+}
+
+int main(int argc, const char * argv[])
+{
+    float x = 0.3;
+    
+    float resultFromH = h(x);
+    double resultFromA = a(x);
+    double_t resultFromB = b(x);
+    float_t resultFromC = c(x);
+
+    cout << "Answer H : " << resultFromH << "\n";
+    cout << "Answer A : " << resultFromA << "\n";
+    cout << "Answer B : " << resultFromB << "\n";
+    cout << "Answer C : " << resultFromC << "\n";
+
+    return 0;
+}
