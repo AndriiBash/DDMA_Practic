16aea5b6a43bf901666386f313a4ae1cf4d7f542
Andrii Izbash <104149691+AndriiBash@users.noreply.github.com>
2024-01-16 12:54:01 +0200

    Update main.cpp

--- LabWork/OOP/1/main.cpp
+++ LabWork/OOP/1/main.cpp
@@ -14,29 +14,29 @@ template <typename type>
 type a(type parametr)
 {
     return 3 * parametr - 2 * pow(cos(parametr), 2);
-}
+} // func a(type parametr) -> type
 
 template <typename type>
 type b(type parametr)
 {
     return log(parametr) + 2 * exp(parametr);
-}
+} // func b(type parametr) -> type
 
 template <typename type>
 type c(type parametr)
 {
     return pow(parametr, 1/3) + 4 * parametr - 1;
-}
+}// func c(type parametr) -> type
 
 template <typename type>
 type h(type parametr)
 {
     return 3 * a(parametr) + 4 * b(parametr) - 8;
-}
+}// func h(type parametr) -> type
 
 int main(int argc, const char * argv[])
 {
-    float x = 0.3;
+    const float x = 0.3;
     
     float resultFromH = h(x);
     double resultFromA = a(x);
@@ -49,4 +49,4 @@ int main(int argc, const char * argv[])
     cout << "Answer C : " << resultFromC << "\n";
 
     return 0;
-}
+}// int main
